7910c2feb59914ae94acad461b1a40b76a353bb5
DanielJ <danielwjameson@yahoo.com>
2017-12-27 01:07:03 -0600

    Add documentation

--- include/Engine/Detail/Detail.hpp
+++ include/Engine/Detail/Detail.hpp
@@ -6,7 +6,14 @@
 #include <string_view>
 #include <ostream>
 
-// TODO: Doc
 namespace Engine::Detail {
+	/**
+	 * @brief Outputs the file, line, and prefix to an output stream.
+	 * @param[in,out] out THe output stream.
+	 * @param[in] prefix The prefix.
+	 * @param[in] file The file.
+	 * @param[in] line The line.
+	 * @return @p out.
+	 */
 	std::ostream& log(std::ostream& out, std::string_view prefix, std::string_view file, int line);
 }
