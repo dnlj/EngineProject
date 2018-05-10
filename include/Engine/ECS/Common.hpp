3fde2ac3f5afc19e6ac2c458036d1bc9eae1b6a2
DanielJ <danielwjameson@yahoo.com>
2018-05-10 14:46:03 -0500

    Remove MAX_COMPONENTS_GLOBAL

--- include/Engine/ECS/Common.hpp
+++ include/Engine/ECS/Common.hpp
@@ -7,9 +7,6 @@ namespace Engine::ECS {
 	/** The maximum number of components registrable. Ideally this would be exactly the number of components used. */
 	constexpr size_t MAX_COMPONENTS = 64;
 
-	/** The maximum number of components registrable across the whole program. */
-	constexpr size_t MAX_COMPONENTS_GLOBAL = 128;
-
 	/** The maximum number of systems registrable. Ideally this would be exactly the number of systems used. */
 	constexpr size_t MAX_SYSTEMS = 64;
 
@@ -31,8 +28,6 @@ namespace Engine::ECS {
 
 // Asserts
 namespace Engine::ECS {
-	static_assert(MAX_COMPONENTS_GLOBAL < std::numeric_limits<ComponentID>::max(), "[Engine::ECS] MAX_COMPONENTS_GLOBAL must be one less than the maximum value for the type ComponentID.");
-	static_assert(MAX_COMPONENTS < MAX_COMPONENTS_GLOBAL, "[Engine::ECS] MAX_COMPONENTS must not be large than MAX_COMPONENTS_GLOBAL.");
-
+	static_assert(MAX_COMPONENTS < std::numeric_limits<ComponentID>::max(), "[Engine::ECS] MAX_COMPONENTS must not be larger than the maximum value for the type ComponentID.");
 	static_assert(MAX_SYSTEMS <= std::numeric_limits<SystemID>::max(), "[Engine::ECS] MAX_SYSTEMS must not be greater than the maximum value for the type SystemID.");
 }
