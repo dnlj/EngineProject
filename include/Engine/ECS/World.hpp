76223e936840aed06338fa86cf664802383642e9
DanielJ <danielwjameson@yahoo.com>
2018-05-13 01:39:10 -0500

    Update ECS::World

--- include/Engine/ECS/World.hpp
+++ include/Engine/ECS/World.hpp
@@ -1,29 +1,43 @@
 #pragma once
 
 // Engine
-#include <Engine/ECS/EntityManager.hpp>
-#include <Engine/ECS/ComponentManager.hpp>
 #include <Engine/ECS/SystemManager.hpp>
+#include <Engine/ECS/ComponentManager.hpp>
+#include <Engine/ECS/EntityManager.hpp>
 
-// TODO: Doc
-// TODO: Enable once SystemManager is fixed
-//namespace Engine::ECS {
-//	class World : private EntityManager, private ComponentManager, private SystemManager {
-//		public:
-//			// EntityManager members
-//			using EntityManager::createEntity;
-//			using EntityManager::destroyEntity;
-//			using EntityManager::isAlive;
-//
-//			// ComponentManager members
-//			using ComponentManager::getComponentID;
-//			using ComponentManager::getBitsetForComponents;
-//			using ComponentManager::registerComponent;
-//
-//			// SystemManager members
-//			using SystemManager::getSystemID;
-//			using SystemManager::getSystem;
-//			using SystemManager::getBitsetForSystems;
-//			using SystemManager::registerSystem;
-//	};
-//}
+
+namespace Engine::ECS {
+	/**
+	 * @tparam SystemsSet The systems for this world to have.
+	 * @tparam ComponentsSet The components for this world to have.
+	 */
+	template<class SystemsSet, class ComponentsSet>
+	class World;
+
+	/** @see World */
+	template<
+		template<class...> class SystemsType,
+		class... Systems,
+		template<class...> class ComponentsType,
+		class... Components
+	> class World<SystemsType<Systems...>, ComponentsType<Components...>>
+		: private SystemManager<SystemsType<Systems...>>
+		, private ComponentManager<ComponentsType<Components...>>
+		, private EntityManager {
+
+		public:
+			// EntityManager members
+			using EntityManager::createEntity;
+			using EntityManager::destroyEntity;
+			using EntityManager::isAlive;
+
+			// ComponentManager members
+			using ComponentManager::getComponentID;
+			using ComponentManager::getBitsetForComponents;
+
+			// SystemManager members
+			using SystemManager::getSystemID;
+			using SystemManager::getSystem;
+			using SystemManager::getBitsetForSystems;
+	};
+}
--- test/Engine/ECS/World.cpp
+++ test/Engine/ECS/World.cpp
@@ -1,19 +1,58 @@
 // Engine
 #include <Engine/ECS/World.hpp>
 
+// Meta
+#include <Meta/TypeSet/TypeSet.hpp>
+
 // GoogleTest
 #include <gtest/gtest.h>
 
-// TODO: Enable once SystemManager is fixed
-//namespace {
-//	class WorldTest : public testing::Test {
-//		public:
-//			WorldTest() {
-//			}
-//
-//			Engine::ECS::World world;
-//	};
-//}
-//
-//TEST_F(WorldTest, Test1) {
-//}
+namespace {
+	template<int I>
+	class Component {
+		public:
+			int value = 0;
+	};
+
+	using ComponentA = Component<0>;
+	using ComponentB = Component<1>;
+	using ComponentC = Component<2>;
+	using ComponentD = Component<3>;
+	using ComponentE = Component<4>;
+
+	template<int I>
+	class System : public Engine::ECS::System {
+		public:
+			int value = 0;
+	};
+
+	using SystemA = System<0>;
+	using SystemB = System<1>;
+	using SystemC = System<2>;
+	using SystemD = System<3>;
+	using SystemE = System<4>;
+
+	using SystemsSet = Meta::TypeSet::TypeSet<
+		SystemA,
+		SystemB,
+		SystemC,
+		SystemD,
+		SystemE
+	>;
+
+	using ComponentsSet = Meta::TypeSet::TypeSet<
+		ComponentA,
+		ComponentB,
+		ComponentC,
+		ComponentD,
+		ComponentE
+	>;
+
+	using W = Engine::ECS::World<SystemsSet, ComponentsSet>;
+}
+
+namespace {
+	TEST(Engine_ECS_World, Constructor) {
+		W w;
+	}
+}
