c68219510f156ac988ab2f6ff40a8f442340d351
DanielJ <danielwjameson@yahoo.com>
2018-05-20 07:18:12 -0500

    Update docs

--- include/Engine/ECS/System.hpp
+++ include/Engine/ECS/System.hpp
@@ -4,16 +4,14 @@
 #include <Engine/ECS/Common.hpp>
 
 namespace Engine::ECS {
-	// TODO: Doc
+	/**
+	 * Abstract class representing a system.
+	 */
 	class System {
 		public:
-			// TODO: Make this use a typeset instead?
-			// TODO: Ideally this would be const/static
 			/** The bitset of systems to have higher priority than. */
 			Engine::ECS::SystemBitset priorityBefore;
 
-			// TODO: Make this use a typeset instead?
-			// TODO: Ideally this would be const/static
 			/** The bitset of systems to have lower priority than. */
 			Engine::ECS::SystemBitset priorityAfter;
 
