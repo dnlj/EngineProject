4d5f1f3ed29ea7d26a2fae2214ecc5749a501229
DanielJ <danielwjameson@yahoo.com>
2018-07-13 21:20:11 -0500

    Change eid to ent

--- test/Engine/ECS/World.cpp
+++ test/Engine/ECS/World.cpp
@@ -59,198 +59,198 @@ namespace {
 	TEST(Engine_ECS_World, create_destroy_Entity) {
 		World w;
 
-		auto eid = w.createEntity();
-		w.destroyEntity(eid);
+		auto ent = w.createEntity();
+		w.destroyEntity(ent);
 	}
 
 	TEST(Engine_ECS_World, getComponent) {
 		World w;
 
-		auto eid = w.createEntity();
+		auto ent = w.createEntity();
 		
-		auto& c1 = w.addComponent<ComponentB>(eid);
-		auto& c2 = w.getComponent<ComponentB>(eid);
+		auto& c1 = w.addComponent<ComponentB>(ent);
+		auto& c2 = w.getComponent<ComponentB>(ent);
 
 		ASSERT_EQ(&c1, &c2);
 
-		auto& c3 = w.getComponent<ComponentB>(eid);
+		auto& c3 = w.getComponent<ComponentB>(ent);
 
 		ASSERT_EQ(&c2, &c3);
 	}
 
 	TEST(Engine_ECS_World, has_add_remove_Component) {
 		World w;
-		auto eid = w.createEntity();
+		auto ent = w.createEntity();
 
-		ASSERT_FALSE(w.hasComponent<ComponentA>(eid));
-		ASSERT_FALSE(w.hasComponent<ComponentB>(eid));
-		ASSERT_FALSE(w.hasComponent<ComponentC>(eid));
-		ASSERT_FALSE(w.hasComponent<ComponentD>(eid));
-		ASSERT_FALSE(w.hasComponent<ComponentE>(eid));
+		ASSERT_FALSE(w.hasComponent<ComponentA>(ent));
+		ASSERT_FALSE(w.hasComponent<ComponentB>(ent));
+		ASSERT_FALSE(w.hasComponent<ComponentC>(ent));
+		ASSERT_FALSE(w.hasComponent<ComponentD>(ent));
+		ASSERT_FALSE(w.hasComponent<ComponentE>(ent));
 
-		w.addComponent<ComponentA>(eid);
+		w.addComponent<ComponentA>(ent);
 
-		ASSERT_TRUE(w.hasComponent<ComponentA>(eid));
-		ASSERT_FALSE(w.hasComponent<ComponentB>(eid));
-		ASSERT_FALSE(w.hasComponent<ComponentC>(eid));
-		ASSERT_FALSE(w.hasComponent<ComponentD>(eid));
-		ASSERT_FALSE(w.hasComponent<ComponentE>(eid));
+		ASSERT_TRUE(w.hasComponent<ComponentA>(ent));
+		ASSERT_FALSE(w.hasComponent<ComponentB>(ent));
+		ASSERT_FALSE(w.hasComponent<ComponentC>(ent));
+		ASSERT_FALSE(w.hasComponent<ComponentD>(ent));
+		ASSERT_FALSE(w.hasComponent<ComponentE>(ent));
 
-		w.addComponent<ComponentC>(eid);
+		w.addComponent<ComponentC>(ent);
 
-		ASSERT_TRUE(w.hasComponent<ComponentA>(eid));
-		ASSERT_FALSE(w.hasComponent<ComponentB>(eid));
-		ASSERT_TRUE(w.hasComponent<ComponentC>(eid));
-		ASSERT_FALSE(w.hasComponent<ComponentD>(eid));
-		ASSERT_FALSE(w.hasComponent<ComponentE>(eid));
+		ASSERT_TRUE(w.hasComponent<ComponentA>(ent));
+		ASSERT_FALSE(w.hasComponent<ComponentB>(ent));
+		ASSERT_TRUE(w.hasComponent<ComponentC>(ent));
+		ASSERT_FALSE(w.hasComponent<ComponentD>(ent));
+		ASSERT_FALSE(w.hasComponent<ComponentE>(ent));
 
-		w.addComponent<ComponentE>(eid);
+		w.addComponent<ComponentE>(ent);
 
-		ASSERT_TRUE(w.hasComponent<ComponentA>(eid));
-		ASSERT_FALSE(w.hasComponent<ComponentB>(eid));
-		ASSERT_TRUE(w.hasComponent<ComponentC>(eid));
-		ASSERT_FALSE(w.hasComponent<ComponentD>(eid));
-		ASSERT_TRUE(w.hasComponent<ComponentE>(eid));
+		ASSERT_TRUE(w.hasComponent<ComponentA>(ent));
+		ASSERT_FALSE(w.hasComponent<ComponentB>(ent));
+		ASSERT_TRUE(w.hasComponent<ComponentC>(ent));
+		ASSERT_FALSE(w.hasComponent<ComponentD>(ent));
+		ASSERT_TRUE(w.hasComponent<ComponentE>(ent));
 
-		w.removeComponent<ComponentA>(eid);
+		w.removeComponent<ComponentA>(ent);
 
-		ASSERT_FALSE(w.hasComponent<ComponentA>(eid));
-		ASSERT_FALSE(w.hasComponent<ComponentB>(eid));
-		ASSERT_TRUE(w.hasComponent<ComponentC>(eid));
-		ASSERT_FALSE(w.hasComponent<ComponentD>(eid));
-		ASSERT_TRUE(w.hasComponent<ComponentE>(eid));
+		ASSERT_FALSE(w.hasComponent<ComponentA>(ent));
+		ASSERT_FALSE(w.hasComponent<ComponentB>(ent));
+		ASSERT_TRUE(w.hasComponent<ComponentC>(ent));
+		ASSERT_FALSE(w.hasComponent<ComponentD>(ent));
+		ASSERT_TRUE(w.hasComponent<ComponentE>(ent));
 
-		w.removeComponent<ComponentC>(eid);
+		w.removeComponent<ComponentC>(ent);
 
-		ASSERT_FALSE(w.hasComponent<ComponentA>(eid));
-		ASSERT_FALSE(w.hasComponent<ComponentB>(eid));
-		ASSERT_FALSE(w.hasComponent<ComponentC>(eid));
-		ASSERT_FALSE(w.hasComponent<ComponentD>(eid));
-		ASSERT_TRUE(w.hasComponent<ComponentE>(eid));
+		ASSERT_FALSE(w.hasComponent<ComponentA>(ent));
+		ASSERT_FALSE(w.hasComponent<ComponentB>(ent));
+		ASSERT_FALSE(w.hasComponent<ComponentC>(ent));
+		ASSERT_FALSE(w.hasComponent<ComponentD>(ent));
+		ASSERT_TRUE(w.hasComponent<ComponentE>(ent));
 
-		w.removeComponent<ComponentE>(eid);
+		w.removeComponent<ComponentE>(ent);
 
-		ASSERT_FALSE(w.hasComponent<ComponentA>(eid));
-		ASSERT_FALSE(w.hasComponent<ComponentB>(eid));
-		ASSERT_FALSE(w.hasComponent<ComponentC>(eid));
-		ASSERT_FALSE(w.hasComponent<ComponentD>(eid));
-		ASSERT_FALSE(w.hasComponent<ComponentE>(eid));
+		ASSERT_FALSE(w.hasComponent<ComponentA>(ent));
+		ASSERT_FALSE(w.hasComponent<ComponentB>(ent));
+		ASSERT_FALSE(w.hasComponent<ComponentC>(ent));
+		ASSERT_FALSE(w.hasComponent<ComponentD>(ent));
+		ASSERT_FALSE(w.hasComponent<ComponentE>(ent));
 	}
 
 	TEST(Engine_ECS_World, hasComponent) {
 		World w;
 
-		const auto eid = w.createEntity();
+		const auto ent = w.createEntity();
 
-		ASSERT_FALSE(w.hasComponent(eid, w.getComponentID<ComponentA>()));
-		ASSERT_FALSE(w.hasComponent(eid, w.getComponentID<ComponentC>()));
-		ASSERT_FALSE(w.hasComponent(eid, w.getComponentID<ComponentE>()));
+		ASSERT_FALSE(w.hasComponent(ent, w.getComponentID<ComponentA>()));
+		ASSERT_FALSE(w.hasComponent(ent, w.getComponentID<ComponentC>()));
+		ASSERT_FALSE(w.hasComponent(ent, w.getComponentID<ComponentE>()));
 
-		w.addComponent<ComponentA>(eid);
-		w.addComponent<ComponentC>(eid);
-		w.addComponent<ComponentE>(eid);
+		w.addComponent<ComponentA>(ent);
+		w.addComponent<ComponentC>(ent);
+		w.addComponent<ComponentE>(ent);
 
-		ASSERT_TRUE(w.hasComponent(eid, w.getComponentID<ComponentA>()));
-		ASSERT_TRUE(w.hasComponent(eid, w.getComponentID<ComponentC>()));
-		ASSERT_TRUE(w.hasComponent(eid, w.getComponentID<ComponentE>()));
+		ASSERT_TRUE(w.hasComponent(ent, w.getComponentID<ComponentA>()));
+		ASSERT_TRUE(w.hasComponent(ent, w.getComponentID<ComponentC>()));
+		ASSERT_TRUE(w.hasComponent(ent, w.getComponentID<ComponentE>()));
 
-		w.removeComponent<ComponentA>(eid);
-		w.removeComponent<ComponentC>(eid);
-		w.removeComponent<ComponentE>(eid);
+		w.removeComponent<ComponentA>(ent);
+		w.removeComponent<ComponentC>(ent);
+		w.removeComponent<ComponentE>(ent);
 
-		ASSERT_FALSE(w.hasComponent(eid, w.getComponentID<ComponentA>()));
-		ASSERT_FALSE(w.hasComponent(eid, w.getComponentID<ComponentC>()));
-		ASSERT_FALSE(w.hasComponent(eid, w.getComponentID<ComponentE>()));
+		ASSERT_FALSE(w.hasComponent(ent, w.getComponentID<ComponentA>()));
+		ASSERT_FALSE(w.hasComponent(ent, w.getComponentID<ComponentC>()));
+		ASSERT_FALSE(w.hasComponent(ent, w.getComponentID<ComponentE>()));
 	}
 
 	TEST(Engine_ECS_World, hasComponent_Template) {
 		World w;
 
-		const auto eid = w.createEntity();
+		const auto ent = w.createEntity();
 
-		ASSERT_FALSE(w.hasComponent<ComponentA>(eid));
-		ASSERT_FALSE(w.hasComponent<ComponentC>(eid));
-		ASSERT_FALSE(w.hasComponent<ComponentE>(eid));
+		ASSERT_FALSE(w.hasComponent<ComponentA>(ent));
+		ASSERT_FALSE(w.hasComponent<ComponentC>(ent));
+		ASSERT_FALSE(w.hasComponent<ComponentE>(ent));
 
-		w.addComponent<ComponentA>(eid);
-		w.addComponent<ComponentC>(eid);
-		w.addComponent<ComponentE>(eid);
+		w.addComponent<ComponentA>(ent);
+		w.addComponent<ComponentC>(ent);
+		w.addComponent<ComponentE>(ent);
 
-		ASSERT_TRUE(w.hasComponent<ComponentA>(eid));
-		ASSERT_TRUE(w.hasComponent<ComponentC>(eid));
-		ASSERT_TRUE(w.hasComponent<ComponentE>(eid));
+		ASSERT_TRUE(w.hasComponent<ComponentA>(ent));
+		ASSERT_TRUE(w.hasComponent<ComponentC>(ent));
+		ASSERT_TRUE(w.hasComponent<ComponentE>(ent));
 
-		w.removeComponent<ComponentA>(eid);
-		w.removeComponent<ComponentC>(eid);
-		w.removeComponent<ComponentE>(eid);
+		w.removeComponent<ComponentA>(ent);
+		w.removeComponent<ComponentC>(ent);
+		w.removeComponent<ComponentE>(ent);
 
-		ASSERT_FALSE(w.hasComponent<ComponentA>(eid));
-		ASSERT_FALSE(w.hasComponent<ComponentC>(eid));
-		ASSERT_FALSE(w.hasComponent<ComponentE>(eid));
+		ASSERT_FALSE(w.hasComponent<ComponentA>(ent));
+		ASSERT_FALSE(w.hasComponent<ComponentC>(ent));
+		ASSERT_FALSE(w.hasComponent<ComponentE>(ent));
 	}
 
 	TEST(Engine_ECS_World, hasComponents) {
 		World w;
 
-		const auto eid = w.createEntity();
-		w.addComponent<ComponentA>(eid);
-		w.addComponent<ComponentC>(eid);
-		w.addComponent<ComponentE>(eid);
+		const auto ent = w.createEntity();
+		w.addComponent<ComponentA>(ent);
+		w.addComponent<ComponentC>(ent);
+		w.addComponent<ComponentE>(ent);
 
 		Engine::ECS::ComponentBitset cbits;
 		cbits[0] = true;
 		cbits[2] = true;
 		cbits[4] = true;
 
-		ASSERT_TRUE(w.hasComponents(eid, cbits));
+		ASSERT_TRUE(w.hasComponents(ent, cbits));
 	}
 
 	TEST(Engine_ECS_World, hasComponents_Template) {
 		World w;
 
-		const auto eid = w.createEntity();
-		w.addComponents<ComponentA, ComponentC, ComponentE>(eid);
+		const auto ent = w.createEntity();
+		w.addComponents<ComponentA, ComponentC, ComponentE>(ent);
 
-		const auto value = w.hasComponents<ComponentA, ComponentC, ComponentE>(eid);
+		const auto value = w.hasComponents<ComponentA, ComponentC, ComponentE>(ent);
 		ASSERT_TRUE(value);
 	}
 
 	TEST(Engine_ECS_World, addComponents) {
 		World w;
 
-		const auto eid = w.createEntity();
-		auto& [a, c, e] = w.addComponents<ComponentA, ComponentC, ComponentE>(eid);
+		const auto ent = w.createEntity();
+		auto& [a, c, e] = w.addComponents<ComponentA, ComponentC, ComponentE>(ent);
 		const auto cbits = w.getBitsetForComponents<ComponentA, ComponentC, ComponentE>();
 
-		ASSERT_TRUE(w.hasComponents(eid, cbits));
-		ASSERT_TRUE(&a == &w.getComponent<ComponentA>(eid));
-		ASSERT_TRUE(&c == &w.getComponent<ComponentC>(eid));
-		ASSERT_TRUE(&e == &w.getComponent<ComponentE>(eid));
+		ASSERT_TRUE(w.hasComponents(ent, cbits));
+		ASSERT_TRUE(&a == &w.getComponent<ComponentA>(ent));
+		ASSERT_TRUE(&c == &w.getComponent<ComponentC>(ent));
+		ASSERT_TRUE(&e == &w.getComponent<ComponentE>(ent));
 	}
 
 	TEST(Engine_ECS_World, removeComponents) {
 		World w;
 
-		const auto eid = w.createEntity();
+		const auto ent = w.createEntity();
 		const auto cbits = w.getBitsetForComponents<ComponentA, ComponentC, ComponentE>();
 
-		w.addComponents<ComponentA, ComponentC, ComponentE>(eid);
+		w.addComponents<ComponentA, ComponentC, ComponentE>(ent);
 
-		ASSERT_TRUE(w.hasComponents(eid, cbits));
+		ASSERT_TRUE(w.hasComponents(ent, cbits));
 
-		w.removeComponents<ComponentA, ComponentC, ComponentE>(eid);
+		w.removeComponents<ComponentA, ComponentC, ComponentE>(ent);
 
-		ASSERT_FALSE(w.hasComponent<ComponentA>(eid));
-		ASSERT_FALSE(w.hasComponent<ComponentC>(eid));
-		ASSERT_FALSE(w.hasComponent<ComponentE>(eid));
+		ASSERT_FALSE(w.hasComponent<ComponentA>(ent));
+		ASSERT_FALSE(w.hasComponent<ComponentC>(ent));
+		ASSERT_FALSE(w.hasComponent<ComponentE>(ent));
 	}
 
 	TEST(Engine_ECS_World, getComponents) {
 		World w;
 
-		const auto eid = w.createEntity();
-		auto& [a1, c1, e1] = w.addComponents<ComponentA, ComponentC, ComponentE>(eid);
-		auto& [a2, c2, e2] = w.getComponents<ComponentA, ComponentC, ComponentE>(eid);
+		const auto ent = w.createEntity();
+		auto& [a1, c1, e1] = w.addComponents<ComponentA, ComponentC, ComponentE>(ent);
+		auto& [a2, c2, e2] = w.getComponents<ComponentA, ComponentC, ComponentE>(ent);
 
 
 		ASSERT_TRUE(&a1 == &a2);
