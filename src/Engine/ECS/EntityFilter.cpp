fadeb0c4574a02f40c494dbcbd42cdd16d25b9c9
DanielJ <danielwjameson@yahoo.com>
2018-07-25 19:52:30 -0500

    Remove unused checks

--- src/Engine/ECS/EntityFilter.cpp
+++ src/Engine/ECS/EntityFilter.cpp
@@ -16,7 +16,6 @@ namespace Engine::ECS {
 
 	void EntityFilter::remove(Entity ent) {
 		auto pos = std::lower_bound(entities.cbegin(), entities.cend(), ent);
-		if (pos == entities.cend() || *pos != ent) { return; }
 		entities.erase(pos);
 	}
 
