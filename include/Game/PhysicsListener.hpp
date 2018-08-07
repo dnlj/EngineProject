fd841da3acfb08dc7a8453b6f13507769e36050e
DanielJ <danielwjameson@yahoo.com>
2018-08-07 05:15:11 -0500

    Update docs

--- include/Game/PhysicsListener.hpp
+++ include/Game/PhysicsListener.hpp
@@ -6,14 +6,29 @@
 
 
 namespace Game {
-	// TODO: Doc
 	class PhysicsListener {
 		public:
+			/**
+			 * Destructor
+			 */
 			virtual ~PhysicsListener() {};
 
+			/**
+			 * Called when contact begins.
+			 * @param[in] dataA The first user data.
+			 * @param[in] dataB The second user data.
+			 */
 			virtual void beginContact(const PhysicsUserData& dataA, const PhysicsUserData& dataB) {};
+
+			/**
+			 * Called when contact ends.
+			 * @param[in] dataA The first user data.
+			 * @param[in] dataB The second user data.
+			 */
 			virtual void endContact(const PhysicsUserData& dataA, const PhysicsUserData& dataB) {};
+
 			//TODO: virtual void preSolve() {};
+
 			//TODO: virtual void postSolve() {};
 	};
 }
