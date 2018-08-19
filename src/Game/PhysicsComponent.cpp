0beb189071f384f1fa6cc052ec5a9cb881310e0c
DanielJ <danielwjameson@yahoo.com>
2018-08-18 20:58:54 -0500

    Remove unneeded checks

--- src/Game/PhysicsComponent.cpp
+++ src/Game/PhysicsComponent.cpp
@@ -10,13 +10,7 @@ namespace Game {
 
 	PhysicsComponent::~PhysicsComponent() {
 		if (destruct) {
-			std::cout << "~PhysicsComponent()\n";
-
-			// TODO: This only needs to be here because we dont have all bodies setup correctly atm
-			if (body && physSys) {
-				std::cout << "~PhysicsComponent() - destroy\n";
-				physSys->destroyBody(body);
-			}
+			physSys->destroyBody(body);
 		}
 	}
 
