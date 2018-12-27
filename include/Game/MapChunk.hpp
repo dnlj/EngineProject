34e222ced9479abe953ab2d10f25e74c0a117818
DanielJ <danielwjameson@yahoo.com>
2018-12-27 17:39:35 -0600

    Add quadtree start

--- include/Game/MapChunk.hpp
+++ include/Game/MapChunk.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+// STD
+#include <cmath>
+
 // GLM
 #include <glm/glm.hpp>
 
@@ -10,6 +13,78 @@
 // TODO: Doc
 namespace Game {
 	class MapChunk {
+		private:
+			// TODO: Move
+			// TODO: Split
+			// TODO: Change everything to use root/child/parent/current terms
+			template<int N>
+			class FlatQuadtree {
+				public:
+					using DepthType = int8_t;
+
+					void set(int current, int currentSize) {
+						if (data[current] != -1) { return; }
+
+						memset(data + current, getDepth(currentSize), currentSize);
+
+						const auto parentSize = currentSize * 4;
+						update(getParentIndex(current, parentSize), parentSize);
+					}
+
+					// TODO: could pass around depth so we dont need to calculate it every time
+					void update(int current, int currentSize) {
+						const auto first  = data[getChildIndex(current, 0, currentSize)];
+						const auto second = data[getChildIndex(current, 1, currentSize)];
+						const auto third  = data[getChildIndex(current, 2, currentSize)];
+						const auto fourth = data[getChildIndex(current, 3, currentSize)];
+						const auto depth = getDepth(currentSize);
+
+						// If all the children are at the same depth and they have not yet been merged
+						if (first == second
+							&& second == third
+							&& third == fourth
+							&& fourth == depth + 1) {
+
+							memset(data + current, depth, currentSize);
+							const auto parentSize = currentSize * 4;
+
+							if (parentSize <= rootSize) {
+								update(getParentIndex(current, parentSize), parentSize);
+							}
+						}
+					}
+
+					DepthType getDepth(int size) const {
+						// depth = log4(maxSize / size)
+						//       = log2(maxSize / size) / 2
+						//
+						// Could also use `ctz(maxSize / size) / 2` since we always have a power of two for max/size
+
+						return static_cast<DepthType>(
+							log2(static_cast<float>(rootSize / size)) / 2
+						);
+					}
+
+					int getChildOffset(int child, int childSize) const {
+						return child * (childSize / 4);
+					}
+
+					int getChildIndex(int current, int child, int childSize) const {
+						// TODO: update to work with size = 1
+						return current + getChildOffset(child, childSize);
+					}
+
+					int getParentIndex(int current, int parentSize) const {
+						return (current / parentSize) * parentSize;
+					}
+
+				private:
+					static_assert(N && !(N & (N - 1)), "Template parameter 'N' must be a power of two.");
+
+					constexpr static int rootSize = N * N;
+					DepthType data[rootSize]{};
+			};
+
 		public:
 			constexpr static MapTile AIR{0, false};
 			constexpr static MapTile DIRT{1, true};
