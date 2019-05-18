e3ba25ba40792bab47a79973c8fe618ad1184aa8
DanielJ <danielwjameson@yahoo.com>
2019-05-18 12:50:47 -0500

    Add Input

---
+++ include/Engine/Input.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+// STD
+#include <cstdint>
+#include <array>
+
+
+namespace Engine {
+	// TODO: move
+	enum class InputType : int8_t {
+		KEYBOARD = 0,
+		MOUSE = 1,
+		GAMEPAD = 2,
+	};
+
+	struct Input {
+		// TODO: add operators for sort
+		InputType type;
+		int code = -1;
+	};
+
+	// TODO: Move
+	class InputSet : public std::array<Input, 4> {
+	};
+}
