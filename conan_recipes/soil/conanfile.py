890fa6f7fda7dc3ab6db7d0ddbbd64b08d777750
DanielJ <danielwjameson@yahoo.com>
2020-02-02 06:05:18 -0600

    Add libs to soil recipe

--- conan_recipes/soil/conanfile.py
+++ conan_recipes/soil/conanfile.py
@@ -27,5 +27,5 @@ class Recipe(ConanFile):
 		self.copy("*.a", src="soil/projects", dst="lib", keep_path=False)
 		
 	def package_info(self):
-		#TODO: libs
-		pass
+		for libname in os.listdir("lib"):
+			self.cpp_info.libs.append(libname)
