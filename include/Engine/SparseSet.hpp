283b07fff1de02ed105f55ec0f7a0f7c665493d3
DanielJ <danielwjameson@yahoo.com>
2020-07-01 02:55:26 -0500

    Change SparseSet to use custom iterator that allows for partial const access

--- include/Engine/SparseSet.hpp
+++ include/Engine/SparseSet.hpp
@@ -2,9 +2,9 @@
 
 // STD
 #include <vector>
-#include <utility>
 #include <type_traits>
-#include <concepts>
+#include <utility>
+#include <tuple>
 
 // Engine
 #include <Engine/Engine.hpp>
@@ -12,28 +12,106 @@
 
 
 namespace Engine {
-	// TODO: move
-	// TODO: name? dense, unique, sequential, key
-	
-
 	template<class Key, class Value, class Hash = IndexHash<Key>>
 	class SparseSet {
 		private:
 			using Index = int32;
 			static constexpr bool IsVoid = std::is_same_v<Value, void>;
 			static constexpr Index invalid = static_cast<Index>(-1);
+			using Val = std::conditional_t<IsVoid, Key, Value>;
 
 			union KeyVal_void {
 				KeyVal_void(const Key& key) : first{key} {}
-				// TODO: want these to be const from iterators. Should not be modified.
 				Key first;
-				Key second;
+				Val second;
+
+				union Public {
+					Public(const KeyVal_void& kv) : first{kv.first} {}
+					const Key first;
+					const Val second;
+				};
 			};
-			using Val = std::conditional_t<IsVoid, Key, Value>;
-			using KeyVal = std::conditional_t<IsVoid, KeyVal_void, std::pair<Key, Val>>;
+
+			class KeyVal_value {
+				public:
+					Key first;
+					Val second;
+
+					struct Public {
+						const Key first;
+						Val second;
+					};
+
+					template<class... Args1, class... Args2>
+					KeyVal_value(std::piecewise_construct_t,
+						const std::tuple<Args1...>& args1,
+						const std::tuple<Args2...>& args2)
+						: KeyVal_value{std::piecewise_construct, args1, args2, std::index_sequence_for<Args1...>{}, std::index_sequence_for<Args2...>{}} {
+					};
+
+				private:
+					template<class... Args1, class... Args2, size_t... I1, size_t... I2>
+					KeyVal_value(std::piecewise_construct_t,
+						const std::tuple<Args1...>& args1,
+						const std::tuple<Args2...>& args2,
+						std::index_sequence<I1...>,
+						std::index_sequence<I2...>)
+						: first(std::forward<Args1>(std::get<I1>(args1))...)
+						, second(std::forward<Args2>(std::get<I2>(args2))...) {
+					};
+			};
+			
+			using KeyVal = std::conditional_t<IsVoid, KeyVal_void, KeyVal_value>;
+
 			Hash hash;
 			std::vector<Index> sparse;
 			std::vector<KeyVal> dense;
+
+			template<class Elem>
+			class IteratorBase {
+				private:
+					friend class SparseSet;
+					using AccessType = typename Elem::Public;
+					Elem* curr;
+					IteratorBase(Elem* init) : curr{init} {}
+
+					// Verify our assumption
+					static_assert(sizeof(Elem) == sizeof(AccessType));
+					static_assert(offsetof(Elem, first) == offsetof(AccessType, first));
+					static_assert(offsetof(Elem, second) == offsetof(AccessType, second));
+
+				public:
+					auto& operator+=(Index i) { curr += i; return *this; }
+					auto& operator-=(Index i) { curr -= i; return *this; }
+
+					friend auto operator+(IteratorBase it, Index n) { return it += n; }
+					friend auto operator-(IteratorBase it, Index n) { return it -= n; }
+
+					friend auto operator+(Index n, IteratorBase it) { return it + n; }
+					friend auto operator-(Index n, IteratorBase it) { return it - n; }
+
+					auto operator-(const IteratorBase& it) { return curr - it.curr; }
+
+					void operator++() { ++curr; }
+					void operator--() { ++curr; }
+
+					auto operator++(int) { return *this + 1; }
+					auto operator--(int) { return *this - 1; }
+
+					auto& operator*() const { return reinterpret_cast<AccessType&>(*curr); }
+					auto* operator->() const { return &*this; }
+					auto& operator[](Index n) const { return *(*this + n); }
+
+					bool operator==(const IteratorBase& other) const { return curr == other.curr; }
+					bool operator!=(const IteratorBase& other) const { return !(*this == other); }
+					bool operator<(const IteratorBase& other) const { return curr < other.curr; }
+					bool operator<=(const IteratorBase& other) const { return curr <= other.curr; }
+					bool operator>=(const IteratorBase& other) const { return curr >= other.curr; }
+					bool operator>(const IteratorBase& other) const { return curr > other.curr; }
+			};
+
+			using Iterator = IteratorBase<KeyVal>;
+			using ConstIterator = IteratorBase<const KeyVal>;
 
 		public:
 			SparseSet(const Hash& hash = Hash()) : hash{hash} {}
@@ -98,28 +176,27 @@ namespace Engine {
 			// TODO: empty
 
 			ENGINE_INLINE auto begin() {
-				// TODO: we need `it.first` to be const. In case of IsVoid both should be const.
-				return dense.begin();
+				return Iterator{dense.data()};
+			}
+
+			ENGINE_INLINE auto cbegin() const {
+				return ConstIterator{dense.data()};
 			}
 
 			ENGINE_INLINE auto begin() const {
 				return cbegin();
 			}
 
-			ENGINE_INLINE auto cbegin() {
-				return dense.cbegin();
+			ENGINE_INLINE auto end() {
+				return Iterator{dense.data() + dense.size()};
 			}
 
-			ENGINE_INLINE auto end() {
-				return dense.end();
+			ENGINE_INLINE auto cend() const {
+				return ConstIterator{dense.data() + dense.size()};
 			}
 
 			ENGINE_INLINE auto end() const {
-				return cend;
-			}
-
-			ENGINE_INLINE auto cend() {
-				return dense.cend();
+				return cend();
 			}
 
 			friend void swap(SparseSet& a, SparseSet& b) {
