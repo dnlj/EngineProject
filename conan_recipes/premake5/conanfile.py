fbbd5e64776772382aaf09be8efb107532283f65
DanielJ <dnlj@users.noreply.github.com>
2021-03-08 15:26:38 -0600

    Change premake5 conan generator to use brackets for keys

--- conan_recipes/premake5/conanfile.py
+++ conan_recipes/premake5/conanfile.py
@@ -3,9 +3,8 @@ import conans.model as model
 import collections
 
 def makeSafeKey(s):
-	# We could also wrap keys like ["key.text.here"] but that isnt very nice as a consumer
-	return s.replace(".", "_")
-	
+	return f'["{s}"]'
+
 def toLua(obj, level = 1, prefix = None):
 	seq = level > 0
 	indent = "\t" * level
@@ -20,7 +19,7 @@ def toLua(obj, level = 1, prefix = None):
 		return ["\"" + obj.replace("\\", "\\\\").replace("\"", "\\\"") + "\""]
 	if isinstance(obj, collections.abc.KeysView):
 		return toLua(list(obj), level)
-		 
+
 	# Table like objects
 	parts = []
 	def addValue(v, k = None):
@@ -31,9 +30,9 @@ def toLua(obj, level = 1, prefix = None):
 			parts.append(" = ")
 		parts.extend(toLua(v, level + 1))
 		parts.append(",\n") if seq else parts.append("\n")
-	
+
 	if seq: parts.append("{\n")
-	
+
 	if isinstance(obj, (list, tuple)):
 		for v in obj:
 			addValue(v)
@@ -45,7 +44,7 @@ def toLua(obj, level = 1, prefix = None):
 			addValue(v, k)
 	else:
 		for k in dir(obj):
-			v = getattr(obj, k)	
+			v = getattr(obj, k)
 			if k.startswith("_") or callable(v): continue
 			addValue(v, k)
 	if seq:
@@ -55,45 +54,45 @@ def toLua(obj, level = 1, prefix = None):
 
 def divider(title):
 	return "-" * 80 + "\n-- " + title + "\n" + "-" * 80 + "\n"
-	
+
 class premake5(model.Generator):
 	@property
 	def filename(self):
 		return "conanbuildinfo.lua"
-		
+
 	@property
 	def content(self):
 		parts = []
-		
+
 		parts.append("local conan = {}\n\n")
-		
+
 		parts.append(divider("Settings"))
-		parts.extend(toLua(self.settings, 0, "conan."))
+		parts.extend(toLua(self.settings, 0, "conan"))
 		parts.append("\n")
-		
+
 		parts.append(divider("Build Info"))
-		parts.extend(toLua(self.deps_build_info, 0, "conan."))
-		
+		parts.extend(toLua(self.deps_build_info, 0, "conan"))
+
 		parts.append("return conan")
-		
+
 		return "".join(parts)
-		
+
 class Package(ConanFile):
 	name = "premake5"
 	description = "Generator for premake5"
 	license = "none"
 	homepage = "none"
 	url = "none"
-	
+
 	def build(self):
 		pass
-	
+
 	def package(self):
 		# TODO: is there any way to disable `WARN: No files in this package!`
 		pass
-	
+
 	def package_id(self):
 		self.info.header_only()
-		
+
 	def package_info(self):
 		pass
