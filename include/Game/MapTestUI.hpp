9e8df1d0538f5cb00a4b1c3b659586ca4b6a5b0a
DanielJ <dnlj@users.noreply.github.com>
2021-03-08 19:21:23 -0600

    Setup basics for map node editor

--- include/Game/MapTestUI.hpp
+++ include/Game/MapTestUI.hpp
@@ -1,15 +1,44 @@
 #pragma once
 
+// STD
+#include <vector>
+
 // Engine
 #include <Engine/Clock.hpp>
 #include <Engine/ImGui/ImGui.hpp>
 #include <imgui_node_editor.h>
+#include <Engine/FlatHashMap.hpp>
+
+// Game
+#include <Game/Common.hpp>
 
 
 namespace Game {
 	class MapTestUI {
 		private:
 			ax::NodeEditor::EditorContext* ctx;
+
+			// 16 bits node id 8 bits input number 8 bits output number
+			using Id = uint64;
+
+			// TODO: rm - just make a union. way easier than getters and setters
+			//constexpr static Id getNodePart(Id id) noexcept { return id & 0xFFFF'FFFF'0000'0000 >> 32; }
+			//constexpr static Id getInputPart(Id id) noexcept { return id & 0x0000'0000'FFFF'0000 >> 16; }
+			//constexpr static Id getOutputPart(Id id) noexcept { return id & 0x0000'0000'0000'FFFF; }
+
+			struct Node {
+				std::vector<Id> inputs;
+				std::vector<Id> outputs;
+				std::vector<Id> links;
+			};
+
+			struct Link {
+				Id fromPinId;
+				Id toPinId;
+			};
+
+			Engine::FlatHashMap<Id, Link> links;
+			Engine::FlatHashMap<Id, Node> nodes;
 
 		public:
 			MapTestUI();
--- src/Game/MapTestUI.cpp
+++ src/Game/MapTestUI.cpp
@@ -2,6 +2,27 @@
 
 namespace ImNode = ax::NodeEditor;
 
+namespace {
+	void drawNode(int& id, const char* title) {
+		ImNode::BeginNode(++id);
+			ImGui::Text(title);
+			ImNode::BeginPin(++id, ImNode::PinKind::Input);
+				ImGui::Text("Input 1");
+			ImNode::EndPin();
+
+			ImGui::SameLine();
+
+			ImNode::BeginPin(++id, ImNode::PinKind::Output);
+				ImGui::Text("Output 1");
+			ImNode::EndPin();
+		ImNode::EndNode();
+	}
+
+	void setStyle() {
+		ImNode::PushStyleColor(ImNode::StyleColor_NodeBg, ImColor(127, 0, 0, 255));
+	}
+}
+
 namespace Game {
 	MapTestUI::MapTestUI() {
 		ctx = ImNode::CreateEditor(nullptr);
@@ -18,37 +39,62 @@ namespace Game {
 		if (!ImGui::Begin("Map Playground", nullptr, ImGuiWindowFlags_None)) { ImGui::End(); return; }
 		ImGui::Text("Test!");
 		ImNode::SetCurrentEditor(ctx);
+		setStyle();
 		ImNode::Begin("Test Editor");
 
+		// TODO: add nodes: BeginPopupContextWindow
+
 		int id = 0;
+		drawNode(id, "First node");
+		drawNode(id, "Second node");
 
-		ImNode::BeginNode(id++);
-			ImGui::Text("First Node");
-			ImNode::BeginPin(id++, ImNode::PinKind::Input);
-				ImGui::Text("Input 1");
-			ImNode::EndPin();
+		if (ImNode::BeginCreate()) {
+			ImNode::PinId pinFromId;
+			if (ImNode::QueryNewNode(&pinFromId)) {
+				if (ImNode::AcceptNewItem()) {
+					ENGINE_LOG("Create Node! ", pinFromId.Get());
+					// TODO: impl
+				}
+			}
 
-			ImGui::SameLine();
+			ImNode::PinId pinToId;
+			if (ImNode::QueryNewLink(&pinFromId, &pinToId)) {
+				if (ImNode::AcceptNewItem()) {
+					ENGINE_LOG("New Link! ", pinFromId.Get(), " --> ", pinToId.Get());
 
-			ImNode::BeginPin(id++, ImNode::PinKind::Output);
-				ImGui::Text("Output 1");
-			ImNode::EndPin();
-		ImNode::EndNode();
+					links.emplace(
+						static_cast<Id>(rand()), // TODO: dont rand
+						Link{pinFromId.Get(), pinToId.Get()}
+					);
+				}
+			}
+		}
+		ImNode::EndCreate();
 
-		ImNode::BeginNode(id++);
-			ImGui::Text("Second Node");
-			ImNode::BeginPin(id++, ImNode::PinKind::Input);
-				ImGui::Text("Input 2");
-			ImNode::EndPin();
+		if (ImNode::BeginDelete()) {
+			ImNode::NodeId nodeId;
+			while (ImNode::QueryDeletedNode(&nodeId)) {
+				ENGINE_LOG("Delete node!");
+				// TODO: impl
+			}
 
-			ImGui::SameLine();
+			ImNode::LinkId linkId;
+			while (ImNode::QueryDeletedLink(&linkId)) {
+				ENGINE_LOG("Delete Link! ", linkId.Get());
+				if (ImNode::AcceptDeletedItem()) {
+					links.erase(linkId.Get());
+				}
+			}
+		}
+		ImNode::EndDelete();
 
-			ImNode::BeginPin(id++, ImNode::PinKind::Output);
-				ImGui::Text("Output 2");
-			ImNode::EndPin();
-		ImNode::EndNode();
+		// Draw links
+		for (auto& [id, link] : links) {
+			ImNode::Link(id, link.fromPinId, link.toPinId);
+		}
 
 		ImNode::End();
+		ImNode::SetCurrentEditor(nullptr);
 		ImGui::End();
 	}
 }
