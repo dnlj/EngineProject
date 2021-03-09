0de2c992712880cc78dbdf983d39784d6be81dd6
DanielJ <dnlj@users.noreply.github.com>
2021-03-09 13:49:57 -0600

    Add node id type for ui

--- include/Game/MapTestUI.hpp
+++ include/Game/MapTestUI.hpp
@@ -6,8 +6,8 @@
 // Engine
 #include <Engine/Clock.hpp>
 #include <Engine/ImGui/ImGui.hpp>
-#include <imgui_node_editor.h>
 #include <Engine/FlatHashMap.hpp>
+#include <imgui_node_editor.h>
 
 // Game
 #include <Game/Common.hpp>
@@ -15,30 +15,86 @@
 
 namespace Game {
 	class MapTestUI {
-		private:
-			ax::NodeEditor::EditorContext* ctx;
+		public:
+			/**
+			 * Bit layout of ids.
+			 * Ids are divided into two 32 bit node-pin pairs.
+			 * If both pairs are present then this is a link id.
+			 * If only the lower pair is present then this is a pin id.
+			 * If only the lower node id is present then this is a node id.
+			 * If only the pin part of either pair is present then this is an invalid id.
+			 * If any node/pin/link id is zero it is an invalid/none id. All id sequences begin at one.
+			 * All pins on a node, both input and ouput, share the same id sequence.
+			 *
+			 * 
+			 *  Second Pin Id     Second Node Id    First Pin Id      First Node Id
+			 * |---------------| |---------------| |---------------| |---------------|
+			 * 00000000'00000000'00000000'00000000'00000000'00000000'00000000'00000000
+			 * |---------------------------------| |---------------------------------|
+			 *	 Second node-pin pair                First node-pin pair
+			 *
+			 */
+			union PinId {
+				uint32 full;
+				struct {
+					uint16 node;
+					uint16 pin;
+				};
+				PinId() : full{} {}
+				PinId(const ax::NodeEditor::PinId& id) : full{static_cast<uint32>(id.Get())} {}
+				operator uint64() const { return full; }
+				operator ax::NodeEditor::PinId() const { return full; }
+			}; static_assert(sizeof(PinId) == 4, "This type is assumed to be tightly packed.");
 
-			// 16 bits node id 8 bits input number 8 bits output number
-			using Id = uint64;
+			union Id {
+				uint64 full;
+				uint16 node;
+				struct {
+					PinId pin1;
+					PinId pin2;
+				};
 
-			// TODO: rm - just make a union. way easier than getters and setters
-			//constexpr static Id getNodePart(Id id) noexcept { return id & 0xFFFF'FFFF'0000'0000 >> 32; }
-			//constexpr static Id getInputPart(Id id) noexcept { return id & 0x0000'0000'FFFF'0000 >> 16; }
-			//constexpr static Id getOutputPart(Id id) noexcept { return id & 0x0000'0000'0000'FFFF; }
+				Id() : full{} {}
+				Id(uint64 id) : full{id} {}
+				Id(const ax::NodeEditor::NodeId& id) : full{id.Get()} {}
+				Id(const ax::NodeEditor::LinkId& id) : full{id.Get()} {}
+				Id(const ax::NodeEditor::PinId& id) : full{id.Get()} {}
+				Id(const ax::NodeEditor::PinId& p1, const ax::NodeEditor::PinId& p2) : pin1{p1.Get()}, pin2{p2.Get()} {}
+
+				operator ax::NodeEditor::NodeId() const { return full; }
+				operator ax::NodeEditor::LinkId() const { return full; }
+				operator ax::NodeEditor::PinId() const { return full; }
+
+				operator uint64() const { return full; }
+
+				std::string string() const { return "[("+ std::to_string(pin2.pin) + ", "+ std::to_string(pin2.node) + "), (" + std::to_string(pin1.pin) + ", "+ std::to_string(pin1.node) + ")]"; }
+
+				struct Hash {
+					ENGINE_INLINE size_t operator()(const Id& id) const noexcept {
+						return static_cast<size_t>(id);
+					}
+				};
+			}; static_assert(sizeof(Id) == 8, "This type is assumed to be tightly packed.");
+
+			struct Pin {
+				bool output = false;
+			};
 
 			struct Node {
-				std::vector<Id> inputs;
-				std::vector<Id> outputs;
+				// Input and output pins share the same id sequence
+				std::vector<Pin> pins;
 				std::vector<Id> links;
 			};
 
 			struct Link {
-				Id fromPinId;
-				Id toPinId;
 			};
 
-			Engine::FlatHashMap<Id, Link> links;
-			Engine::FlatHashMap<Id, Node> nodes;
+		private:
+			ax::NodeEditor::EditorContext* ctx;
+			Id lastNodeId = 0;
+
+			Engine::FlatHashMap<Id, Link, Id::Hash> links;
+			Engine::FlatHashMap<Id, Node, Id::Hash> nodes;
 
 		public:
 			MapTestUI();
--- src/Game/MapTestUI.cpp
+++ src/Game/MapTestUI.cpp
@@ -1,30 +1,35 @@
 #include <Game/MapTestUI.hpp>
+#include <imgui_node_editor_internal.h> // TODO: rm for debugging
 
 namespace ImNode = ax::NodeEditor;
 
 namespace {
-	void drawNode(int& id, const char* title) {
-		ImNode::BeginNode(++id);
+	void drawNode(Game::MapTestUI::Id id, const char* title) {
+		ImNode::BeginNode(id);
 			ImGui::Text(title);
-			ImNode::BeginPin(++id, ImNode::PinKind::Input);
+			id.pin1.pin = 1;
+			ImNode::BeginPin(id, ImNode::PinKind::Input);
 				ImGui::Text("Input 1");
 			ImNode::EndPin();
-
+			
 			ImGui::SameLine();
-
-			ImNode::BeginPin(++id, ImNode::PinKind::Output);
+			
+			id.pin1.pin = 2;
+			ImNode::BeginPin(id, ImNode::PinKind::Output);
 				ImGui::Text("Output 1");
 			ImNode::EndPin();
 		ImNode::EndNode();
 	}
 
 	void setStyle() {
-		ImNode::PushStyleColor(ImNode::StyleColor_NodeBg, ImColor(127, 0, 0, 255));
+		//ImNode::PushStyleColor(ImNode::StyleColor_NodeBg, ImColor(127, 0, 0, 255));
+		//ImNode::PushStyleVar(ImNode::StyleVar_NodeRounding, 1.0f);
 	}
 }
 
 namespace Game {
 	MapTestUI::MapTestUI() {
+		//ImNode::Config cfg; // TODO: file, save/load
 		ctx = ImNode::CreateEditor(nullptr);
 	};
 
@@ -44,28 +49,29 @@ namespace Game {
 
 		// TODO: add nodes: BeginPopupContextWindow
 
-		int id = 0;
-		drawNode(id, "First node");
-		drawNode(id, "Second node");
+		//int id = 0;
+		//drawNode(id, "First node");
+		//drawNode(id, "Second node");
 
 		if (ImNode::BeginCreate()) {
 			ImNode::PinId pinFromId;
 			if (ImNode::QueryNewNode(&pinFromId)) {
 				if (ImNode::AcceptNewItem()) {
 					ENGINE_LOG("Create Node! ", pinFromId.Get());
-					// TODO: impl
+					
+					ImNode::Suspend();
+					ImGui::OpenPopup("New Node");
+					ImNode::Resume();
 				}
 			}
 
 			ImNode::PinId pinToId;
 			if (ImNode::QueryNewLink(&pinFromId, &pinToId)) {
+				// TODO: that one id is an input and the other is an output
 				if (ImNode::AcceptNewItem()) {
-					ENGINE_LOG("New Link! ", pinFromId.Get(), " --> ", pinToId.Get());
-
-					links.emplace(
-						static_cast<Id>(rand()), // TODO: dont rand
-						Link{pinFromId.Get(), pinToId.Get()}
-					);
+					Id link{pinFromId, pinToId};
+					ENGINE_LOG("New Link! ", link.string());
+					links.emplace(link, Link{});
 				}
 			}
 		}
@@ -75,23 +81,83 @@ namespace Game {
 			ImNode::NodeId nodeId;
 			while (ImNode::QueryDeletedNode(&nodeId)) {
 				ENGINE_LOG("Delete node!");
-				// TODO: impl
+				nodes.erase(nodeId);
 			}
 
 			ImNode::LinkId linkId;
 			while (ImNode::QueryDeletedLink(&linkId)) {
-				ENGINE_LOG("Delete Link! ", linkId.Get());
+				ENGINE_LOG("Delete Link!");
 				if (ImNode::AcceptDeletedItem()) {
-					links.erase(linkId.Get());
+					links.erase(linkId);
 				}
 			}
 		}
 		ImNode::EndDelete();
-
-		// Draw links
-		for (auto& [id, link] : links) {
-			ImNode::Link(id, link.fromPinId, link.toPinId);
+		
+		// Render nodes
+		// Nodes must be rendered BEFORE links
+		for (auto& [id, node] : nodes) {
+			std::string label = "Node " + std::to_string(id);
+			drawNode(id, label.c_str());
 		}
+
+		// Render links
+		// Links must be rendered AFTER nodes
+		for (auto& [id, link] : links) {
+			ImNode::Link(id, id.pin1, id.pin2);
+		}
+
+		// Context Menus
+		ImNode::Suspend();
+		{
+			if (ImNode::NodeId id; ImNode::ShowNodeContextMenu(&id)) {
+				ENGINE_LOG("Node context");
+				ImGui::OpenPopup("Test Popup");
+			} else if (ImNode::PinId id; ImNode::ShowPinContextMenu(&id)) {
+				ENGINE_LOG("Pin context");
+				ImGui::OpenPopup("Test Popup");
+			} else if (ImNode::LinkId id; ImNode::ShowLinkContextMenu(&id)) {
+				ENGINE_LOG("Link context");
+				ImGui::OpenPopup("Test Popup");
+			} else if (ImNode::ShowBackgroundContextMenu()) {
+				ENGINE_LOG("Background context");
+				ImGui::OpenPopup("Test Popup");
+			}
+
+			if (ImGui::BeginPopup("Test Popup")) {
+				ImGui::Text("Test~~~~~~~~~~~~~~");
+
+				if (ImGui::Button("Create Node")) {
+					ImGui::CloseCurrentPopup();
+					++lastNodeId.node;
+					nodes[lastNodeId] = {};
+				}
+
+				ImGui::EndPopup();
+			}
+
+			if (ImGui::BeginPopup("New Node")) {
+				ImGui::Button("Constant");
+				ImGui::Button("Gradient");
+				ImGui::Button("Worley Noise");
+				ImGui::Button("Open Simplex Noise");
+				ImGui::Button("White Noise");
+				ImGui::Separator();
+				ImGui::Button("Add");
+				ImGui::Button("Subtract");
+				ImGui::Button("Multiply");
+				ImGui::Button("Divide");
+				ImGui::Button("Modulus");
+				ImGui::Button("DivMod");
+				ImGui::Separator();
+				ImGui::Button("Min");
+				ImGui::Button("Max");
+				ImGui::Button("Abs");
+				ImGui::Button("Lerp");
+				ImGui::EndPopup();
+			}
+		}
+		ImNode::Resume();
 
 		ImNode::End();
 		ImNode::SetCurrentEditor(nullptr);
