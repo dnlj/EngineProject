a35bfbc2d67f28697ebb348b3ee1745219404406
DanielJ <dnlj@users.noreply.github.com>
2021-10-13 18:10:02 -0500

    Remove old functions

--- include/Game/systems/ActionSystem.hpp
+++ include/Game/systems/ActionSystem.hpp
@@ -2,12 +2,6 @@
 // STD
 #include <string>
 
-// Engine
-#include <Engine/Clock.hpp>
-#include <Engine/FlatHashMap.hpp>
-#include <Engine/Input/ActionListener.hpp>
-#include <Engine/Net/Connection.hpp>
-
 // Game
 #include <Game/System.hpp>
 #include <Game/comps/ActionComponent.hpp>
@@ -15,9 +9,6 @@
 
 namespace Game {
 	class ActionSystem : public System {
-		private:
-			std::vector<std::vector<Engine::Input::ActionListener>> actionIdToListeners;
-
 		public:
 			ActionSystem(SystemArg arg);
 
@@ -30,16 +21,9 @@ namespace Game {
 			void updateTarget(glm::vec2 val);
 			void updateTarget(Engine::ECS::Entity ent, glm::vec2 val);
 
-			template<class Listener>
-			void addListener(Engine::Input::ActionId aid, Listener&& listener);
-
-			Engine::Input::ActionId count() const;
-
 		public:
 			void recvActions(Connection& from, const Engine::Net::MessageHeader& head, Engine::ECS::Entity fromEnt);
 			void recvActionsClient(Connection& from, const Engine::Net::MessageHeader& head, Engine::ECS::Entity fromEnt);
 			void recvActionsServer(Connection& from, const Engine::Net::MessageHeader& head, Engine::ECS::Entity fromEnt);
 	};
 }
-
-#include <Game/systems/ActionSystem.ipp>
