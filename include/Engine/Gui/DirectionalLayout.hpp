c9f9acb42d7a1ffea61ea6101fd2591f59cb8373
DanielJ <dnlj@users.noreply.github.com>
2021-11-07 19:47:20 -0600

    DirectionalLayout cleanup

--- include/Engine/Gui/DirectionalLayout.hpp
+++ include/Engine/Gui/DirectionalLayout.hpp
@@ -6,19 +6,25 @@
 
 
 namespace Engine::Gui {
-	enum class Direction {
-		Horizontal,
-		Vertical,
-		_count,
+	struct Direction_ {
+		enum class Direction {
+			Horizontal,
+			Vertical,
+			_count,
+		};
 	};
+	using Direction = Direction_::Direction;
 
-	enum class Align {
-		Start,
-		End,
-		Center,
-		Stretch,
-		_count,
+	struct Align_ {
+		enum Align {
+			Start,
+			End,
+			Center,
+			Stretch,
+			_count,
+		};
 	};
+	using Align = Align_::Align;
 
 	class DirectionalLayout : public Layout {
 		private:
@@ -87,17 +93,17 @@ namespace Engine::Gui {
 				glm::vec2 cpos = panel->getPos();
 
 				// Only used for main axis stretch.
-				// TODO: is there a better way to handle these single case variables?
-				float32 totalWeight = weight;
-				int32 count = 0;
-				// TODO: rm - no longer used - float32 gapAdj = 0;
-
 				struct StretchData {
 					float32 min = 0;
 					float32 max = 0;
 					float32 val = 0;
 					float32 weight = 0;
 				};
+				// TODO: is there a better way to handle these single case variables?
+				float32 totalWeight = weight;
+				int32 count = 0;
+				int stretchIndex = -1;
+				std::vector<StretchData> stretchData;
 
 				if (mainAlign == Align::Start) {
 					// Already set correctly
@@ -109,40 +115,28 @@ namespace Engine::Gui {
 					ENGINE_WARN("TODO: impl Align::Center for main axis");
 				} else if (mainAlign == Align::Stretch) {
 					const bool accum = totalWeight == 0;
-					const auto old = curr;
 					while (curr) {
 						++count;
 						totalWeight += accum ? curr->getWeight() : 0;
-						curr = (curr->*next)();
+						curr = curr->getNextSibling();
 					}
-					curr = old;
-					// TODO: rm - gapAdj = gap * (count - 1) / count;
-					// TODO: rm - size[main] -= gap * (count - 1);
-				} else [[unlikely]] {
-					ENGINE_WARN("Unknown layout main axis alignment");
-				}
-					
-				// TODO: better way to handle these single use vairables
-				int stretchIndex = -1;
-				std::vector<StretchData> stretchData;
+					curr = panel->getFirstChild();
 
-				if (mainAlign == Align::Stretch) {
-					// TODO: data population could be moved above
 					stretchData.resize(count);
-					const auto old = curr;
-					for (int i = 0; curr; ++i, curr = (curr->*next)()) {
+					for (int i = 0; curr; ++i, curr = curr->getNextSibling()) {
 						stretchData[i] = {
 							.min = curr->getMinSize()[main],
 							.max = curr->getMaxSize()[main],
-							.val = 0,
+							.val = 0, // TODO: init to min, also subtract from remSize
 							.weight = curr->getWeight(),
 						};
 					}
-					curr = old;
+					curr = panel->getFirstChild();
 
-					// TODO: move gap changes to size[main] into this calc and make size const again
 					float32 remSize = size[main] - gap * (count - 1);
 					float32 remWeight = totalWeight;
+
+					// TODO: Also need to check that fixed totalWeight still works
 
 					// TODO: seems like there may be an issue with needing extra iterations?
 					// TODO: resize of an empty collapsible section should only take 1 iter? it says two.
@@ -170,52 +164,28 @@ namespace Engine::Gui {
 						}
 					}
 					ENGINE_LOG("Iters: ", iter, " ", panel);
+				} else [[unlikely]] {
+					ENGINE_WARN("Unknown layout main axis alignment");
 				}
 
 				// TODO: could we pull some of the switching out of the loop? lambda? how does that compile down?
+				const auto stretch = mainAlign == Align::Stretch || crossAlign == Align::Stretch;
 				while (curr) {
 					auto pos = cpos;
 
-					// TODO: refactor main/cross align so we dont resize twice
-					if (mainAlign == Align::Stretch) {
-						// This is arguably incorrect because we distribute the gap space
-						// evenly between all panels. This causes things to be slightly off
-						// from what you might expect. For example with two panels 2:1
-						// the panel sizes themself will not be quite 2:1. It is the total
-						// `panelSize + gap*(n-1)/n` which is 2:1.
-						//
-						// The other way to do things would be to take the total space then
-						// subtract (n-1)*gap from it then divide that between all panels.
-						// You would also need to use this new gap adjusted size to calc
-						// individual panels weight adjusted size. Overall this makes more
-						// intuitive sense but isnt necessarily more correct.
-						//
-						// The second way (removing gaps before dividing size) doesnt work
-						// well with fixed layout weighting, whereas the first does.
-						//
-
+					if (stretch) {
 						auto sz = curr->getSize();
-						sz[main] = stretchData[++stretchIndex].val;
+
+						if (mainAlign == Align::Stretch) {
+							// Use pre build data
+							sz[main] = stretchData[++stretchIndex].val;
+						}
+
+						if (crossAlign == Align::Stretch) {
+							sz[cross] = size[cross];
+						}
+
 						curr->setSize(sz);
-
-						//////const auto w = curr->getWeight() / totalWeight;
-						//////auto sz = curr->getSize();
-
-						//sz[main] = w * size[main] - gapAdj;
-						//////sz[main] = w * size[main];
-						//////curr->setSize(sz);
-
-						//
-						//
-						//
-						// TODO: this still doesnt work because what if a later item has a smaller size? we dont go back and resize
-						// older elements
-						//
-						//
-						//
-
-						//////totalWeight -= curr->getWeight();
-						//////size[main] -= curr->getSize()[main];
 					}
 					
 					if (mainAlign == Align::End) {
@@ -224,12 +194,6 @@ namespace Engine::Gui {
 						pos[main] -= diff;
 					} else {
 						cpos[main] += curr->getSize()[main] + gap;
-					}
-
-					if (crossAlign == Align::Stretch) {
-						auto sz = curr->getSize();
-						sz[cross] = size[cross];
-						curr->setSize(sz);
 					}
 
 					if (crossAlign == Align::Start) {
