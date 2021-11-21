9d1440e31c006a959e67e119ae72ce6488a03db4
DanielJ <dnlj@users.noreply.github.com>
2021-11-21 16:20:21 -0600

    Add Engine::ArrayView

---
+++ include/Engine/ArrayView.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+// Engine
+#include <Engine/Engine.hpp>
+
+
+namespace Engine {
+	template<class T>
+	class ArrayView {
+		private:
+			T* dataBegin = nullptr;
+			T* dataEnd = nullptr;
+
+		public:
+			ArrayView() = default;
+
+			ENGINE_INLINE ArrayView(T* begin, T* end)
+				: dataBegin{begin}
+				, dataEnd{end} {
+				ENGINE_DEBUG_ASSERT(dataBegin <= dataEnd);
+			};
+
+			ENGINE_INLINE ArrayView(T* data, ptrdiff_t size)
+				: ArrayView(data, data + size) {
+			};
+
+			ENGINE_INLINE ArrayView(auto& cont)
+				: ArrayView(adl_data(cont), adl_size(cont)) {
+			}
+
+			ENGINE_INLINE ArrayView(std::initializer_list<T> list)
+				: ArrayView(list.begin(), list.end()) {
+			}
+
+			ENGINE_INLINE T* begin() noexcept { return dataBegin; }
+			ENGINE_INLINE const T* begin() const noexcept { return dataBegin; }
+			ENGINE_INLINE const T* cbegin() const noexcept { return dataBegin; }
+
+			ENGINE_INLINE T* end() noexcept { return dataEnd; }
+			ENGINE_INLINE const T* end() const noexcept { return dataEnd; }
+			ENGINE_INLINE const T* cend() const noexcept { return dataEnd; }
+
+			// TODO: rbegin
+			// TODO: crbegin
+			// TODO: rend
+			// TODO: crend
+
+			ENGINE_INLINE bool empty() const noexcept { return dataBegin == dataEnd; }
+			ENGINE_INLINE auto size() const noexcept { return dataEnd - dataBegin; }
+
+			ENGINE_INLINE T* data() noexcept { return dataBegin; }
+			ENGINE_INLINE const T* data() const noexcept { return dataBegin; }
+	};
+}
