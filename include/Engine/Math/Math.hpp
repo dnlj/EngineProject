279c94a363762b831895e27c3703232db597203d
DanielJ <dnlj@users.noreply.github.com>
2021-12-20 05:09:47 -0600

    Add rsqrt/rsqrt0

--- include/Engine/Math/Math.hpp
+++ include/Engine/Math/Math.hpp
@@ -1,4 +1,6 @@
 #pragma once
+// Windows
+#include <intrin.h>
 
 // STD
 #include <concepts>
@@ -8,5 +10,25 @@ namespace Engine::Math {
 	template<class T, std::floating_point F>
 	ENGINE_INLINE constexpr T lerp(T a, T b, F t) {
 		return t * a + (F{1} - t) * b;
+	}
+
+	/**
+	 * Rough estimate of the inverse square root.
+	 * @see rsqrt
+	 */
+	ENGINE_INLINE float rsqrt0(const float x) {
+		return _mm_cvtss_f32(_mm_rsqrt_ss(_mm_set_ss(x)));
+	}
+
+	/**
+	 * Estimate of the inverse square root.
+	 * Uses one step of Newton's method to give a more refined result than rsqrt0.
+	 * This is 2.5x as fast as using std::sqrt and very close in accuracy.
+	 * See benchmark "rsqrt" for details.
+	 * @see rsqrt0
+	 */
+	ENGINE_INLINE float rsqrt(const float x) {
+		const auto est = rsqrt0(x);
+		return est * (1.5f - x * 0.5f * est * est);
 	}
 }
