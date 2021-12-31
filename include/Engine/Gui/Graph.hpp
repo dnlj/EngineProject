95a05288afa5b4acf4f39150530cb62ef9757c31
DanielJ <dnlj@users.noreply.github.com>
2021-12-31 17:51:09 -0600

    Split gui graph into cpp file

--- include/Engine/Gui/Graph.hpp
+++ include/Engine/Gui/Graph.hpp
@@ -4,6 +4,7 @@
 #include <Engine/Gui/Panel.hpp>
 #include <Engine/Gfx/Mesh.hpp>
 #include <Engine/Math/Math.hpp>
+#include <Engine/Gui/Context.hpp>
 
 
 // TODO: split
@@ -44,41 +45,7 @@ namespace Engine::Gui {
 
 	class AreaGraph : public SubGraph {
 		public:
-			void draw(const Panel* panel) const {
-				if (data.empty()) { return; }
-				auto ctx = panel->getContext();
-				const glm::vec2 scale = panel->getSize() / (max - min);
-				const auto h = panel->getHeight();
-
-				const auto end = data.cend();
-				auto curr = data.cbegin();
-				auto prev = curr;
-
-				while (curr != end && curr->x <= min.x) { ++curr; }
-				if (curr == end) { return; }
-				if (curr == prev) { ++curr; } else { prev = curr - 1; }
-
-				while (curr != end) {
-					if (prev->x > max.x) { break; }
-
-					glm::vec2 points[] = {
-						*prev - min,
-						*curr - min,
-						glm::vec2{curr->x - min.x, 0},
-						glm::vec2{prev->x - min.x, 0},
-					};
-
-					for (auto& p : points) {
-						p.x *= scale.x;
-						p.y = h - p.y * scale.y;
-					}
-
-					ctx->drawPoly(points, color);
-
-					prev = curr;
-					++curr;
-				}
-			};
+			void draw(const Panel* panel) const;;
 	};
 
 	class LineGraph : public SubGraph {
@@ -87,74 +54,7 @@ namespace Engine::Gui {
 			float32 thickness = 10;
 
 		public:
-			void draw(const Panel* panel) const {
-				const auto end = data.cend();
-				auto curr = data.cbegin();
-				if (end - curr < 2) { return; }
-
-				// TODO: atm we cull first last point to soon. we need to overdraw by one point to get full miters
-				while (curr != end && curr->x <= min.x) { ++curr; }
-				if (curr == end) { return; }
-				if (curr == data.cbegin()) { ++curr; }
-
-				auto ctx = panel->getContext();
-				const glm::vec2 scale = panel->getSize() / (max - min);
-				const auto h = panel->getHeight();
-
-				// TODO: move into base subgraph class. All graph classes will need something like this.
-				// TODO: use lowp_fvec2 for fast norm?
-				const auto worldToGraph = [&](auto p) ENGINE_INLINE {
-					p = (p - min) * scale;
-					p.y = h - p.y;
-					return p;
-				};
-
-				const auto miterLength = [&](auto aTan, auto bTan) ENGINE_INLINE {
-					const auto sinHalfAngleInv = Math::rsqrt(0.5f + 0.5f * glm::dot(aTan, bTan));
-					return thickness * sinHalfAngleInv;
-				};
-
-				const auto nextMiterPoints = [&](auto point, auto pointT, auto lastT) ENGINE_INLINE {
-					const auto mT = glm::normalize(pointT + lastT);
-					const auto mN = miterLength(pointT, lastT) * glm::vec2{-mT.y, mT.x};
-					return std::array{point - mN, point + mN};
-				};
-
-				// xV = x's vector, xT = x's tangent, xN = x's normal
-				const auto pV = worldToGraph(curr[-1]);
-				auto cV = worldToGraph(*curr);
-				auto cT = glm::normalize(cV - pV);
-				auto pT = cT;
-				auto next = curr + 1;
-				glm::vec2 nV = {};
-				auto [a2, a1] = nextMiterPoints(pV, cT, pT);
-
-				int i = 0; srand(0xDEADBEEF); // TODO: rm
-				while (true) {
-					cV = worldToGraph(*curr);
-					if (next != end) {
-						nV = worldToGraph(*next);
-						cT = glm::normalize(nV - cV);
-					}
-
-					auto [a3, a4] = nextMiterPoints(cV, cT, pT);
-
-					ctx->drawPoly({a1,a2,a3,a4},/*color*/glm::vec4{rand()%256/255.0f,rand()%256/255.0f,rand()%256/255.0f,0.5});
-					++i;
-
-					if (next == end) { break; }
-					if (curr->x > max.x) { break; }
-
-					a1 = a4;
-					a2 = a3;
-					pT = cT;
-					cV = nV;
-					curr = next;
-					++next;
-				};
-
-				//ENGINE_INFO("Draw Poly x", i);
-			};
+			void draw(const Panel* panel) const;
 	};
 
 	class Graph : public Panel {
@@ -166,80 +66,15 @@ namespace Engine::Gui {
 			Graph(Context* context) : Panel{context} {
 			}
 
-			// TODO: do we want an getPoint function like imgui or do we want our own addPoint function?
-			// The benefit of doing an addPoint function would be we know exactly when and what data
-			// is updated vs polling and doing a full rebuild every frame.
-			// The downside is that we have to store duplicate data.
-			//
-			// I think i want to to the addPoint way. how to handle culling data?
+			virtual void render() const override;
 
-			virtual void render() const override {
-				ctx->drawRect({0,0}, getSize(), {0,1,0,0.2});
-				// TODO: these really should be rendered at +1 depth for correct clipping
-				for (auto& graph : graphs) {
-					graph->draw(this);
-				}
-			}
+			virtual void onAction(ActionEvent act) override;
 
-			virtual void onAction(ActionEvent act) override {
-				// TODO: should we scale scroll by SPI_GETWHEELSCROLLLINES/SPI_GETWHEELSCROLLCHARS?
-				switch (act) {
-					case Action::Scroll: {
-						// Reduce scroll speed
-						const auto s = act.value.f32 * 0.5f;
+			virtual void onBeginActivate() override;
 
-						if (s < 0) {
-							scale(1 - s);
-						} else {
-							scale(1.0f / (1 + s));
-						}
+			virtual void onEndActivate() override;
 
-						break;
-					}
-					case Action::ScrollH: {
-						break;
-					}
-					default: { break; }
-				}
-			}
-
-			// TODO: i think we really want this on right click, maybe this should be an action?
-			virtual void onBeginActivate() override {
-				if (ctx->getActive() == this) { return; }
-
-				lastDragPos = ctx->getCursor();
-				ctx->registerMouseMove(this, [this](const glm::vec2 pos) {
-					const auto sz = getSize();
-					const auto diff = glm::vec2{lastDragPos.x - pos.x, pos.y - lastDragPos.y};
-					for (auto& graph : graphs) {
-						const auto scale = (graph->max - graph->min) / sz;
-						const auto offset = diff * scale;
-						graph->min += offset;
-						graph->max += offset;
-					}
-					lastDragPos = pos;
-				});
-			}
-
-			virtual void onEndActivate() override {
-				ctx->deregisterMouseMove(this);
-			}
-
-			void scale(float32 s) {
-				auto p = ctx->getCursor();
-				p -= getPos();
-				p /= getSize();
-				p.y = 1.0f - p.y;
-				p = glm::clamp(p, 0.0f, 1.0f);
-
-				for (auto& graph : graphs) {
-					auto size = graph->max - graph->min;
-					const auto target = p * size + graph->min; // cursor in world space
-					size *= s;
-					graph->min = target - size * p;
-					graph->max = graph->min + size;
-				}
-			}
+			void scale(float32 s);
 
 		private:
 	};
---
+++ src/Engine/Gui/Graph.cpp
@@ -0,0 +1,182 @@
+
+// Engine
+#include <Engine/Gui/Graph.hpp>
+
+
+namespace Engine::Gui {
+	void AreaGraph::draw(const Panel* panel) const {
+		if (data.empty()) { return; }
+		auto ctx = panel->getContext();
+		const glm::vec2 scale = panel->getSize() / (max - min);
+		const auto h = panel->getHeight();
+
+		const auto end = data.cend();
+		auto curr = data.cbegin();
+		auto prev = curr;
+
+		while (curr != end && curr->x <= min.x) { ++curr; }
+		if (curr == end) { return; }
+		if (curr == prev) { ++curr; } else { prev = curr - 1; }
+
+		while (curr != end) {
+			if (prev->x > max.x) { break; }
+
+			glm::vec2 points[] = {
+				*prev - min,
+				*curr - min,
+				glm::vec2{curr->x - min.x, 0},
+				glm::vec2{prev->x - min.x, 0},
+			};
+
+			for (auto& p : points) {
+				p.x *= scale.x;
+				p.y = h - p.y * scale.y;
+			}
+
+			ctx->drawPoly(points, color);
+
+			prev = curr;
+			++curr;
+		}
+	};
+
+	void LineGraph::draw(const Panel* panel) const {
+		const auto end = data.cend();
+		auto curr = data.cbegin();
+		if (end - curr < 2) { return; }
+
+		// TODO: atm we cull first last point to soon. we need to overdraw by one point to get full miters
+		while (curr != end && curr->x <= min.x) { ++curr; }
+		if (curr == end) { return; }
+		if (curr == data.cbegin()) { ++curr; }
+
+		auto ctx = panel->getContext();
+		const glm::vec2 scale = panel->getSize() / (max - min);
+		const auto h = panel->getHeight();
+
+		// TODO: move into base subgraph class. All graph classes will need something like this.
+		// TODO: use lowp_fvec2 for fast norm?
+		const auto worldToGraph = [&](auto p) ENGINE_INLINE {
+			p = (p - min) * scale;
+			p.y = h - p.y;
+			return p;
+		};
+
+		const auto miterLength = [&](auto aTan, auto bTan) ENGINE_INLINE {
+			const auto sinHalfAngleInv = Math::rsqrt(0.5f + 0.5f * glm::dot(aTan, bTan));
+			return thickness * sinHalfAngleInv;
+		};
+
+		const auto nextMiterPoints = [&](auto point, auto pointT, auto lastT) ENGINE_INLINE {
+			const auto mT = glm::normalize(pointT + lastT);
+			const auto mN = miterLength(pointT, lastT) * glm::vec2{-mT.y, mT.x};
+			return std::array{point - mN, point + mN};
+		};
+
+		// xV = x's vector, xT = x's tangent, xN = x's normal
+		const auto pV = worldToGraph(curr[-1]);
+		auto cV = worldToGraph(*curr);
+		auto cT = glm::normalize(cV - pV);
+		auto pT = cT;
+		auto next = curr + 1;
+		glm::vec2 nV = {};
+		auto [a2, a1] = nextMiterPoints(pV, cT, pT);
+
+		int i = 0; srand(0xDEADBEEF); // TODO: rm
+		while (true) {
+			cV = worldToGraph(*curr);
+			if (next != end) {
+				nV = worldToGraph(*next);
+				cT = glm::normalize(nV - cV);
+			}
+
+			auto [a3, a4] = nextMiterPoints(cV, cT, pT);
+
+			ctx->drawPoly({a1,a2,a3,a4},/*color*/glm::vec4{rand()%256/255.0f,rand()%256/255.0f,rand()%256/255.0f,0.5});
+			++i;
+
+			if (next == end) { break; }
+			if (curr->x > max.x) { break; }
+
+			a1 = a4;
+			a2 = a3;
+			pT = cT;
+			cV = nV;
+			curr = next;
+			++next;
+		};
+
+		//ENGINE_INFO("Draw Poly x", i);
+	};
+}
+
+namespace Engine::Gui {
+	void Graph::render() const {
+		ctx->drawRect({0,0}, getSize(), {0,1,0,0.2});
+		// TODO: these really should be rendered at +1 depth for correct clipping
+		for (auto& graph : graphs) {
+			graph->draw(this);
+		}
+	}
+
+	void Graph::onAction(ActionEvent act) {
+		// TODO: should we scale scroll by SPI_GETWHEELSCROLLLINES/SPI_GETWHEELSCROLLCHARS?
+		switch (act) {
+			case Action::Scroll: {
+				// Reduce scroll speed
+				const auto s = act.value.f32 * 0.5f;
+
+				if (s < 0) {
+					scale(1 - s);
+				} else {
+					scale(1.0f / (1 + s));
+				}
+
+				break;
+			}
+			case Action::ScrollH: {
+				break;
+			}
+			default: { break; }
+		}
+	}
+
+	// TODO: i think we really want this on right click, maybe this should be an action?
+	void Graph::onBeginActivate() {
+		if (ctx->getActive() == this) { return; }
+
+		// TODO: i think we really want this on right/middle click, maybe this should be an action?
+		lastDragPos = ctx->getCursor();
+		ctx->registerMouseMove(this, [this](const glm::vec2 pos) {
+			const auto sz = getSize();
+			const auto diff = glm::vec2{lastDragPos.x - pos.x, pos.y - lastDragPos.y};
+			for (auto& graph : graphs) {
+				const auto scale = (graph->max - graph->min) / sz;
+				const auto offset = diff * scale;
+				graph->min += offset;
+				graph->max += offset;
+			}
+			lastDragPos = pos;
+		});
+	}
+
+	void Graph::onEndActivate() {
+		ctx->deregisterMouseMove(this);
+	}
+
+	void Graph::scale(float32 s) {
+		auto p = ctx->getCursor();
+		p -= getPos();
+		p /= getSize();
+		p.y = 1.0f - p.y;
+		p = glm::clamp(p, 0.0f, 1.0f);
+
+		for (auto& graph : graphs) {
+			auto size = graph->max - graph->min;
+			const auto target = p * size + graph->min; // cursor in world space
+			size *= s;
+			graph->min = target - size * p;
+			graph->max = graph->min + size;
+		}
+	}
+}
