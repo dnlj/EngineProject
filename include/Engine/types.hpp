ce5eb18b9d9ddae8a216b25b02536cc88babea3a
DanielJ <dnlj@users.noreply.github.com>
2022-02-27 15:35:07 -0600

    Rename Engine/types and add fast int types

--- include/Engine/types.hpp
+++ include/Engine/types.hpp
@@ -18,10 +18,20 @@ namespace Engine::Types { // TODO: C++20: namespace Engine::inline Types {
 	using int32 = int32_t;
 	using int64 = int64_t;
 
+	using intf8 = int_fast8_t;
+	using intf16 = int_fast16_t;
+	using intf32 = int_fast32_t;
+	using intf64 = int_fast64_t;
+
 	using uint8 = uint8_t;
 	using uint16 = uint16_t;
 	using uint32 = uint32_t;
 	using uint64 = uint64_t;
+
+	using uintf8 = uint_fast8_t;
+	using uintf16 = uint_fast16_t;
+	using uintf32 = uint_fast32_t;
+	using uintf64 = uint_fast64_t;
 
 	using float32 = float;
 	static_assert(sizeof(float32) == 4, "float32 is an incorrect number of bytes.");
