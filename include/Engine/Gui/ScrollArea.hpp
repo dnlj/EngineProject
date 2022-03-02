deacdbe15c861109e8be6a7eaa4fbdab91083370
DanielJ <dnlj@users.noreply.github.com>
2022-03-01 22:08:46 -0600

    Make scrollbar center when clicking in track

--- include/Engine/Gui/ScrollArea.hpp
+++ include/Engine/Gui/ScrollArea.hpp
@@ -60,7 +60,9 @@ namespace Engine::Gui {
 			virtual void onBeginActivate() {
 				const auto abs = getPos()[D];
 				const auto cur = ctx->getCursor()[D];
-				if (cur < abs + p || cur > abs + p + s) { return; }
+				if (cur < abs + p || cur > abs + p + s) {
+					setScrollOffset(cur - abs - s * 0.5f);
+				}
 
 				ctx->registerMouseMove(this, [this, init=p, last=cur](glm::vec2 pos) noexcept {
 					setScrollOffset(init + pos[D] - last);
