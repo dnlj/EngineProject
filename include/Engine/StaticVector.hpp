6e65259990d45898f5dbd4e7f6682d12ef8b5aee
DanielJ <dnlj@users.noreply.github.com>
2022-04-14 01:54:05 -0500

    Inline static vector members

--- include/Engine/StaticVector.hpp
+++ include/Engine/StaticVector.hpp
@@ -43,100 +43,104 @@ namespace Engine {
 			}
 
 			// TODO: overloads with default value
-			void resize(size_type count) noexcept {
+			ENGINE_INLINE void resize(size_type count) noexcept {
 				//ENGINE_ASSERT(count <= capacity(), "Cannot resize larger than capacity() = " << capacity());
 				used = count;
 			}
 
 			// TODO: clear
 
-			T& front() noexcept {
+			ENGINE_INLINE T& front() noexcept {
 				return storage[0];
 			}
 
-			const T& front() const noexcept {
+			ENGINE_INLINE const T& front() const noexcept {
 				return storage[0];
 			}
 
-			T& back() noexcept {
+			ENGINE_INLINE T& back() noexcept {
 				return storage[used - 1];
 			}
 
-			const T& back() const noexcept {
+			ENGINE_INLINE const T& back() const noexcept {
 				return storage[used - 1];
 			}
 
-			size_type size() const noexcept {
+			ENGINE_INLINE size_type size() const noexcept {
 				return used;
 			}
 
-			T* data() noexcept {
+			ENGINE_INLINE T* data() noexcept {
 				return storage;
 			}
 
-			bool empty() const noexcept {
+			ENGINE_INLINE const T* data() const noexcept {
+				return storage;
+			}
+
+			ENGINE_INLINE bool empty() const noexcept {
 				return used == 0;
 			}
 
-			constexpr static size_type capacity() noexcept {
+			ENGINE_INLINE constexpr static size_type capacity() noexcept {
 				return N;
 			}
 
-			constexpr static size_type max_size() noexcept {
+			ENGINE_INLINE constexpr static size_type max_size() noexcept {
 				return capacity();
 			}
 
-			T& operator[](const size_type i) noexcept {
+			ENGINE_INLINE T& operator[](const size_type i) noexcept {
 				return storage[i];
 			}
 
-			const T& operator[](const size_type i) const {
+			ENGINE_INLINE const T& operator[](const size_type i) const {
 				return storage[i];
 			}
 
-			iterator begin() noexcept {
+			ENGINE_INLINE iterator begin() noexcept {
 				return storage;
 			}
 
-			const_iterator begin() const noexcept {
+			ENGINE_INLINE const_iterator begin() const noexcept {
 				return storage;
 			}
 
-			const_iterator cbegin() const noexcept {
+			ENGINE_INLINE const_iterator cbegin() const noexcept {
 				return storage;
 			}
 
-			iterator end() noexcept {
+			ENGINE_INLINE iterator end() noexcept {
 				return storage + used;
 			}
 
-			const_iterator end() const noexcept {
+			ENGINE_INLINE const_iterator end() const noexcept {
 				return storage + used;
 			}
 
-			const_iterator cend() const noexcept {
+			ENGINE_INLINE const_iterator cend() const noexcept {
 				return storage + used;
 			}
 
 			// TODO: (c)rbegin()
 			// TODO: (c)rend()
 
-			void expand(size_type n = 1) noexcept {
+			ENGINE_INLINE void expand(size_type n = 1) noexcept {
 				resize(size() + n);
 			}
 
-			void push_back(T t) {
+			ENGINE_INLINE void push_back(T t) {
 				ENGINE_DEBUG_ASSERT(size() < capacity());
 				expand();
 				back() = t;
 			}
 
-			void pop_back() {
+			ENGINE_INLINE void pop_back() {
 				ENGINE_DEBUG_ASSERT(size() > 0);
 				resize(size() - 1);
 			}
 
-			void erase(iterator it) {
+			ENGINE_INLINE void erase(iterator it) {
 				std::move(it + 1, end(), it);
 				pop_back();
 			}
