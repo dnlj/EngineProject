032068f63a86d965574470b5e077ba36a4093a21
DanielJ <dnlj@users.noreply.github.com>
2022-09-15 07:05:11 -0500

    Change anim system to use correct entity positions

--- src/Game/systems/AnimSystem.cpp
+++ src/Game/systems/AnimSystem.cpp
@@ -1,15 +1,16 @@
 // Engine
 #include <Engine/Camera.hpp>
 #include <Engine/Gfx/BufferManager.hpp>
-#include <Engine/Gfx/ModelLoader.hpp>
-#include <Engine/Gfx/Mesh2.hpp>
 #include <Engine/Gfx/Material.hpp>
+#include <Engine/Gfx/Mesh2.hpp>
+#include <Engine/Gfx/ModelLoader.hpp>
 
 
 // Game
-#include <Game/systems/AnimSystem.hpp>
-#include <Game/comps/ModelComponent.hpp>
 #include <Game/comps/ArmatureComponent.hpp>
+#include <Game/comps/ModelComponent.hpp>
+#include <Game/comps/PhysicsInterpComponent.hpp>
+#include <Game/systems/AnimSystem.hpp>
 
 
 namespace Game {
@@ -31,10 +32,16 @@ namespace Game {
 
 		const auto& data = engine.getModelLoader().get(fileName);
 
+		constexpr static float32 inc = 5;
+		float32 xOff = std::size(ents) * 0.5f * -inc;
 		for (auto& ent : ents) {
 			ent = world.createEntity();
 			auto& mdlComp = world.addComponent<ModelComponent>(ent, data);
 			auto& armComp = world.addComponent<ArmatureComponent>(ent);
+			auto& physInterpComp = world.addComponent<PhysicsInterpComponent>(ent);
+
+			physInterpComp.trans.p.x = xOff;
+			xOff += inc;
 
 			armComp = data.arm;
 
@@ -94,10 +101,10 @@ namespace Game {
 		auto& cam = engine.getCamera();
 		glm::mat4 vpT = cam.getProjection() * cam.getView();
 
-		constexpr static float32 inc = 5;
-		glm::mat4 mT = glm::translate(glm::mat4{1.0f}, glm::vec3{-inc * std::size(ents) * 0.5f, 0, 0});
+		//constexpr static float32 inc = 5;
+		//glm::mat4 mT = glm::translate(glm::mat4{1.0f}, glm::vec3{-inc * std::size(ents) * 0.5f, 0, 0});
 
-		const auto& armFilter = world.getFilter<ModelComponent, ArmatureComponent>(); // TODO: cache in system
+		const auto& armFilter = world.getFilter<ModelComponent, ArmatureComponent, PhysicsInterpComponent>(); // TODO: cache in system
 
 		constexpr static auto align256 = [](const auto v) ENGINE_INLINE -> decltype(v) {
 			return (v & ~0xFF); // floor(x / 256) * 256
@@ -131,6 +138,9 @@ namespace Game {
 			ENGINE_DEBUG_ASSERT(offset < (1<<16), "Too many bones in armature.");
 			ENGINE_DEBUG_ASSERT(sz < (1<<16), "Too many bones in armature.");
 
+			const auto& pos = world.getComponent<PhysicsInterpComponent>(ent).getPosition();
+			const auto mT = glm::translate(glm::mat4{1.0f}, glm::vec3{pos.x, pos.y, 0});
+
 			// TODO: really need a way to set a binding point per model as well as per mesh.  kinda feels like we are leaking draw commands at this point.
 			for (auto& inst : mdl.meshes) {
 				// We could just use baseInstance instead of an id buffer, but that will only work until we get multi draw setup
@@ -143,7 +153,6 @@ namespace Game {
 			}
 
 			offset += sz;
-			mT = glm::translate(mT, glm::vec3{inc,0,0});
 		}
 
 		if (auto sz = bonesBuffTemp.size(); sz > bonesBuffSize) {
