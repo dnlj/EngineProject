eaf0ff8505469df6b9086e138d849e5b738cb7ac
DanielJ <dnlj@users.noreply.github.com>
2022-10-10 12:05:06 -0500

    Add check to skip empty chunks when rendering

--- src/Game/systems/MapRenderSystem.cpp
+++ src/Game/systems/MapRenderSystem.cpp
@@ -52,11 +52,12 @@ namespace Game {
 				const auto found = mapSys.activeChunks.find({x, y});
 				if (found == mapSys.activeChunks.cend()) { continue; }
 				const auto& data = found->second;
+				if (data.ecount == 0) { continue; }
 
 				const auto pos = data.body->GetPosition();
 				const auto mvp = glm::translate(vp, glm::vec3(pos.x, pos.y, 0.0f));
 				glUniformMatrix4fv(0, 1, GL_FALSE, &mvp[0][0]);
-				
+
 				glVertexArrayVertexBuffer(vao, 0, data.vbuff.get(), 0, sizeof(MapSystem::Vertex));
 				glVertexArrayElementBuffer(vao, data.ebuff.get());
 				glDrawElements(GL_TRIANGLES, data.ecount, GL_UNSIGNED_SHORT, 0);
