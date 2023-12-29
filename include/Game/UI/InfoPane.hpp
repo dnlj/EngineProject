172fb1669b7a3b7658382eb84f065c57db9c94db
DanielJ <dnlj@users.noreply.github.com>
2023-12-29 10:18:53 -0600

    Add run delta to info pane

--- include/Game/UI/InfoPane.hpp
+++ include/Game/UI/InfoPane.hpp
@@ -23,6 +23,7 @@ namespace Game::UI {
 				Tick,
 				TickScale,
 				TickRate,
+				RunDelta,
 			};
 
 			InfoPane(EUI::Context* context);
--- src/Game/UI/InfoPane.cpp
+++ src/Game/UI/InfoPane.cpp
@@ -19,6 +19,7 @@ namespace Game::UI {
 		addLabel("Tick: {}");
 		addLabel("Tick Scale: {:.3f}");
 		addLabel("Tick Rate: {}");
+		addLabel("Run Delta: {}");
 		setLabel(UI::InfoPane::TickRate, Game::tickrate);
 
 		disconnect = ctx->constructPanel<EUI::Button>();
@@ -149,5 +150,6 @@ namespace Game::UI {
 
 		setLabel(UI::InfoPane::Tick, world.getTick());
 		setLabel(UI::InfoPane::TickScale, world.tickScale);
+		setLabel(UI::InfoPane::RunDelta, std::chrono::duration<double, std::milli>{world.getDeltaTimeNS()});
 	}
 }
